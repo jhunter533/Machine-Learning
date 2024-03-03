f28aa14d1a5f87da986674abd8ff728a456efc84
jhunter533 <jhunter533@gmail.com>
2024-03-03 01:02:07 -0700

    update

--- emnistTest2.py
+++ emnistTest2.py
@@ -30,21 +30,30 @@ def plotImage(images,predicted_labels,actual_labels):
     plt.show()
 
 model=tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)),
+    
+    tf.keras.layers.Conv2D(32,(5,5),activation='relu',input_shape=(28,28,1)),
+    tf.keras.layers.Conv2D(32,(5,5),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Dropout(.5),
-    tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
+   # tf.keras.layers.Dropout(.5),
+    tf.keras.layers.Conv2D(128,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.Conv2D(128,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Dropout(.5),
+   # tf.keras.layers.Dropout(.5),
   #  tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
    # tf.keras.layers.Dropout(.5),
+    tf.keras.layers.Conv2D(256,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.Conv2D(256,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128,activation='relu'),
     tf.keras.layers.Dropout(.5),
     tf.keras.layers.Dense(47,activation='softmax')
     ])
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-model.fit(xTrain,yTrain,epochs=3)
+model.fit(xTrain,yTrain,epochs=10)
 model.evaluate(xTest,yTest)
 
 model.summary()
